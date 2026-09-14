7be04721e72b2ed1b396970103888a2c0188e9d9
birdycrazy <227081272+birdcrazy@users.noreply.github.com>
2026-09-13 20:22:10 -0400

    Change color markdown wrapper to avoid appearing as a valid link

--- public/js/modules/app-admin.js
+++ public/js/modules/app-admin.js
@@ -2514,7 +2514,7 @@ _handleMarkdownShortcuts(inputEl, event) {
   // Text color (Ctrl/Cmd + Shift + F) (F for font, since c for code is already taken)
   if (event.shiftKey && key === 'f') {
     const colorPrefix = 'c#(51,153,255)'
-    if (!this._wrapSelectedText(input, colorPrefix, '#c')) return false;
+    if (!this._wrapSelectedText(input, colorPrefix, ';c')) return false;
 
     // Move cursor to just before ")" so the user can edit the color
     const cursorPos = input.selectionStart + (colorPrefix.length - 1);
